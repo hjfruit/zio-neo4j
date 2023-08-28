3d9506b597e865363870d5a5e10f49e722726c6d
梦境迷离 <dreamylost@outlook.com>
2023-08-28 18:16:38 +0800

    opt

--- core/src/main/scala/zio/neo4j/impl/Neo4jTransactionLive.scala
+++ core/src/main/scala/zio/neo4j/impl/Neo4jTransactionLive.scala
@@ -8,10 +8,10 @@ import org.neo4j.driver.async.AsyncTransaction
 
 final class Neo4jTransactionLive(underlying: AsyncTransaction) extends Neo4jTransaction:
 
-  override def close: Task[Unit] = ZIO.fromCompletionStage(underlying.closeAsync()).unit
+  override def close: Task[Unit] = ZIO.blocking(ZIO.fromCompletionStage(underlying.closeAsync()).unit)
 
-  override def commit: Task[Unit] = ZIO.fromCompletionStage(underlying.commitAsync()).unit
+  override def commit: Task[Unit] = ZIO.blocking(ZIO.fromCompletionStage(underlying.commitAsync()).unit)
 
-  override def rollback: Task[Unit] = ZIO.fromCompletionStage(underlying.rollbackAsync()).unit
+  override def rollback: Task[Unit] = ZIO.blocking(ZIO.fromCompletionStage(underlying.rollbackAsync()).unit)
 
 end Neo4jTransactionLive
